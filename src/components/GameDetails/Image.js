cc55c9120ad3e81ef7a4aac24ea3cafa217b53f8
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2021-07-19 19:04:53 +0100

    Added image popup when the user clicks on a image

--- src/components/GameDetails/Image.js
+++ src/components/GameDetails/Image.js
@@ -1,14 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import useKeypress from "../../hooks/useKeypress";
 import styled from "styled-components";
 
 const Image = ({ image, alt }) => {
-	return <StyledImage image={getImage(image)} alt={alt} />;
+	const [imageModalState, setImageModelState] = useState(false);
+
+	const openImageModalHandler = () => {
+		document.body.style.overflow = "hidden";
+		setImageModelState(true);
+	};
+
+	const closeImageModalHandler = (e) => {
+		if (e !== undefined) {
+			if (e.target.classList.contains("background")) {
+				document.body.style.overflow = "auto";
+				setImageModelState(false);
+			}
+		} else {
+			document.body.style.overflow = "auto";
+			setImageModelState(false);
+		}
+	};
+
+	useKeypress("Escape", closeImageModalHandler);
+
+	return (
+		<>
+			<StyledImage
+				onClick={openImageModalHandler}
+				image={getImage(image)}
+				alt={alt}
+			/>
+			{imageModalState && (
+				<ImageModalBackground
+					className="background"
+					onClick={closeImageModalHandler}
+				>
+					<ModalImage image={getImage(image)} alt={alt} />
+				</ImageModalBackground>
+			)}
+		</>
+	);
 };
 
 const StyledImage = styled((props) => <GatsbyImage {...props} />)`
 	margin-bottom: 1rem;
 	border-radius: 10px;
+	cursor: pointer;
+`;
+
+const ImageModalBackground = styled.div`
+	width: 100%;
+	padding: 0rem 5rem;
+	min-height: 100vh;
+	background: rgba(0, 0, 0, 0.5);
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 10;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
+	@media (max-width: 780px) {
+		padding: 0rem 2rem;
+	}
+`;
+
+const ModalImage = styled((props) => <GatsbyImage {...props} />)`
+	border-radius: 10px;
+	max-width: 100%;
+	cursor: default;
 `;
 
 export default Image;
