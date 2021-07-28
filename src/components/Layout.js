db4f5e6133bc7647ac217dda9b969d3d044b94ce
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2021-07-28 21:37:42 +0100

    Updated page transition animation length

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -47,6 +47,7 @@ const Layout = ({ pageTitle, children }) => {
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
 					exit={{ opacity: 0 }}
+					transition={{ duration: 0.3 }}
 				>
 					{children}
 				</motion.div>
